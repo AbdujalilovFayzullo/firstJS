1e7f7f3af956ddda3e1452b28deb6d013a7ae12c
Fayzulin694 <abdujalilovfayzullo0407@gmail.com>
2023-05-06 16:24:06 +0500

    First homework in JS

---
+++ main.js
@@ -0,0 +1,11 @@
+let userName = prompt('What is your name')
+
+let lastName = prompt('What is your lastname')
+
+let homeTown = prompt('what is your home town')
+
+let age = prompt('How old are you')
+
+console.log(userName, lastName, homeTown, age)
+
+
